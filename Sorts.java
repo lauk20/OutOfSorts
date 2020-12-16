2f443e814200af7e116a16b3d48ec818340141b2
lauk20 <klau20@stuy.edu>
2020-12-16 12:12:29 -0500

    Fixed bubbleSort() error and add Tester. Not fully tested yet, but sorts.

--- Sorts.java
+++ Sorts.java
@@ -1,10 +1,12 @@
+import java.util.*;
+
 public class Sorts{
   /**Bubble sort of an int array.
   *@postcondition The array will be modified such that the elements will be in increasing order.
   *@param data  the elements to be sorted.
   */
   public static void bubbleSort(int[] data){
-    boolean swapped = false;
+    boolean swapped = true;
 
     for (int i = 0; i < data.length - 1 && swapped; i++){
       swapped = false;
@@ -16,7 +18,9 @@ public class Sorts{
 
           swapped = true;
         }
+        System.out.println(Arrays.toString(data)); //REMOVE WHEN DONE
       }
+      System.out.println(); //REMOVE WHEN DONE
     }
   }
 }
---
+++ Tester.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Tester{
+  public static void main(String[] args){
+    int[] bubbleSort1 = new int[]{5,1,12,-5,16};
+    Sorts.bubbleSort(bubbleSort1);
+    /*for (int i = 0; i < 1000; i++){
+      ArrayList<Integer> numbers = new ArrayList<Integer>;
+
+    }*/
+  }
+}
